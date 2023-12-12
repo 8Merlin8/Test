38f017052ff2e33960e98ad77dbdb9f05e7b1e6b
8Merlin8 <101246781+8Merlin8@users.noreply.github.com>
2023-12-12 15:29:29 +0200

    LVL1

--- Assets/Skript/LineForce.cs
+++ Assets/Skript/LineForce.cs
@@ -1,116 +1,98 @@
 using UnityEngine;
 
-public class LineForce : MonoBehaviour
-{
+public class LineForce : MonoBehaviour {
     [SerializeField] private float shotPower;
-    [SerializeField] private float stopVelocity = 0.05f;
-    [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private Transform startPosition;
+    [SerializeField] private float stopVelocity = .05f; //The velocity below which the rigidbody will be considered as stopped
+
+	[SerializeField] private LineRenderer lineRenderer;
 
     private bool isIdle;
     private bool isAiming;
-    private new Rigidbody rigidbody; // Используем "new" для скрытия предупреждения
 
-    private void Awake()
-    {
+    private Rigidbody rigidbody;
+
+    private void Awake() {
         rigidbody = GetComponent<Rigidbody>();
+
         isAiming = false;
         lineRenderer.enabled = false;
     }
 
-    private void FixedUpdate()
-    {
-        if (rigidbody.velocity.magnitude < stopVelocity)
-        {
+    private void FixedUpdate() {
+        if(rigidbody.velocity.magnitude < stopVelocity) {
             Stop();
         }
 
         ProcessAim();
     }
 
-    private void ProcessAim()
-    {
-        if (!isIdle)
-        {
+    private void OnMouseDown() {
+        if (isIdle) {
+            isAiming = true;
+        }
+    }
+
+    private void ProcessAim() {
+        if(!isAiming || !isIdle) {
             return;
         }
 
-        Vector3 targetPoint = CastMouseClickRay();
+        Vector3? worldPoint = CastMouseClickRay();
 
-        DrawLine(targetPoint);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            isAiming = true;
+        if (!worldPoint.HasValue) {
+            return;
         }
 
-        if (isAiming && Input.GetMouseButtonUp(0))
-        {
-            isAiming = false;
-            Shoot(targetPoint);
+        DrawLine(worldPoint.Value);
+
+        if (Input.GetMouseButtonUp(0)) {
+            Shoot(worldPoint.Value);
         }
     }
 
-    private void Shoot(Vector3 targetPoint)
-    {
+    private void Shoot(Vector3 worldPoint) {
+        isAiming = false;
         lineRenderer.enabled = false;
 
-        Vector3 direction = (targetPoint - transform.position).normalized;
-        direction.y = 0f; // Убираем компоненту по оси Y
+        Vector3 horizontalWorldPoint = new Vector3(worldPoint.x, transform.position.y, worldPoint.z);
 
-        // Set gravity to false while aiming
-        rigidbody.useGravity = false;
+        Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+        float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
 
-        rigidbody.AddForce(direction * shotPower, ForceMode.Impulse);
-
-        // Reset gravity after the shot
-        rigidbody.useGravity = true;
+        rigidbody.AddForce(direction * strength * shotPower);
         isIdle = false;
     }
 
-    private void DrawLine(Vector3 targetPoint)
-    {
-        // Обновляем позицию линии в каждом кадре
-        Vector3 horizontalTargetPoint = new Vector3(targetPoint.x, transform.position.y, targetPoint.z);
+    private void DrawLine(Vector3 worldPoint) {
         Vector3[] positions = {
             transform.position,
-            horizontalTargetPoint
-        };
+            worldPoint};
         lineRenderer.SetPositions(positions);
         lineRenderer.enabled = true;
     }
 
-    private void Stop()
-    {
+    private void Stop() {
         rigidbody.velocity = Vector3.zero;
         rigidbody.angularVelocity = Vector3.zero;
         isIdle = true;
     }
 
-    private Vector3 CastMouseClickRay()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    private Vector3? CastMouseClickRay() {
+        Vector3 screenMousePosFar = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.farClipPlane);
+        Vector3 screenMousePosNear = new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.nearClipPlane);
+        Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+        Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
         RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, float.PositiveInfinity))
-        {
+        if (Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity)) {
             return hit.point;
-        }
-        else
-        {
-            return transform.position; // Возвращаем текущую позицию мяча, если луч не попал
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Cube"))
-        {
-            // Телепортировать мяч на стартовую позицию
-            transform.position = startPosition.position;
-            rigidbody.velocity = Vector3.zero;
-            rigidbody.angularVelocity = Vector3.zero;
-            isIdle = true;
+        } else {
+            return null;
         }
     }
 }
